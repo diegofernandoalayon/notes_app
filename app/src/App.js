4762a9a40dabebc99e72130e95f59553e69cec76
Diego Fernando Alayon <dfar.41@gmail.com>
2021-12-11 14:28:56 -0500

    usando react router v6 para empezar a trabajar con rutas en nuestra app

--- app/src/App.js
+++ app/src/App.js
@@ -1,127 +1,36 @@
-import React, { useEffect, useState } from 'react'
-import Notification from './components/Notification'
-import { FormNote } from './components/FormNote'
+// import { useState } from 'react'
+import { Link, BrowserRouter, Route, Routes } from 'react-router-dom'
+import Notes from './Notes'
 
-import Note from './components/Note/Note'
-import noteService from './services/notes'
-import loginService from './services/login'
+const Home = () => <h1>Home Page</h1>
 
-// import './estilos.css'
-import './index.css'
-import FormLogin from './components/FormLogin'
-// import Toggleable from './components/Toggleable'
+const Users = () => <h1>Users</h1>
+
+const inlineStyle = {
+  padding: 5
+
+}
 
 const App = () => {
-  const [notes, setNotes] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(null)
-
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [user, setUser] = useState(null)
-
-  useEffect(() => {
-    setLoading(true)
-
-    noteService
-      .getAllNotes().then((data) => {
-      // console.log(data)
-        setNotes(data)
-        setLoading(false)
-      })
-  }, [])
-
-  useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem('loggedNoteAppUser')
-    if (loggedUserJSON) {
-      const user = JSON.parse(loggedUserJSON)
-      setUser(user)
-      noteService.setToken(user.token)
-    }
-  }, [])
-
-  const addNote = (noteObject) => {
-    // const {token} = user
-    noteService
-      .createNote(noteObject)
-      .then((data) => {
-        setNotes(prevNotes => prevNotes.concat(data))
-        // setNewNote('')
-      }).catch(error => {
-        console.error(error)
-        setError('La API fallo')
-      })
-  }
-  const toggleImportanceOf = (id) => {
-    const note = notes.find(n => n.id === id)
-    const changedNote = { ...note, important: !note.important }
-
-    noteService
-      .updateNote(id, changedNote)
-      .then(returnedNote => {
-        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
-      })
-    // console.log(changedNote)
-  }
-  const handleLoginSubmit = (event) => {
-    event.preventDefault()
-    loginService.login({
-      username,
-      password
-    }).then((user) => {
-      window.localStorage.setItem(
-        'loggedNoteAppUser', JSON.stringify(user)
-      )
-
-      noteService.setToken(user.token)
-      setUser(user)
-      setUsername('')
-      setPassword('')
-    }).catch(() => {
-      setError('Wrong credentials')
-      setUsername('')
-      setPassword('')
-      setTimeout(() => {
-        setError(null)
-      }, 5000)
-    })
-  }
-  const handleLogout = () => {
-    window.localStorage.removeItem('loggedNoteAppUser')
-    setUser(null)
-    noteService.setToken(user.token)
-  }
-
   return (
-    <div>
-      <h1>Notes</h1>
-      {loading ? 'cargando...' : ''}
-      {/* {error ? error:''} */}
-      <Notification message={error} />
-      {
-        user
-          ? <button onClick={handleLogout}>Logout</button>
-          : null
-      }
-      {
-        user
-          ? <FormNote addNote={addNote} />
-          : <FormLogin
-              username={username}
-              password={password}
-              handleLoginSubmit={handleLoginSubmit}
-              handleUsernameChange={({ target }) => setUsername(target.value)}
-              handlePasswordChange={({ target }) => setPassword(target.value)}
-            />
-      }
+    <BrowserRouter>
 
-      <ol>
-        {notes.map(note => (
-          <Note key={note.id} toggleImportance={() => toggleImportanceOf(note.id)} {...note} />
-        ))}
-      </ol>
+      <header>
 
-    </div>
+        <Link to='/' style={inlineStyle}>Home</Link>
+        <Link to='/notes' style={inlineStyle}>Notes</Link>
+        <Link to='/users' style={inlineStyle}>Users</Link>
+      </header>
+
+      <Routes>
+
+        <Route path='/' element={<Home />} />
+        <Route path='/notes' element={<Notes />} />
+        <Route path='/users' element={<Users />} />
+        <Route path='/*' element={<h1>no disponible</h1>} />
+      </Routes>
+
+    </BrowserRouter>
   )
 }
 
